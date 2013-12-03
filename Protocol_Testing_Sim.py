492cb0ab48d6861aa1d7f2a99c60317684ea257e
lyammine <leilayammine@gmail.com>
2013-12-02 22:45:02 -0600

    Added main function, unfinished bug fixes

--- Protocol_Testing_Sim.py
+++ Protocol_Testing_Sim.py
@@ -14,13 +14,7 @@ gatewaySocket = socket(AF_INET, SOCK_DGRAM)
 gatewayAddress = ("",3000)
 gatewaySocket.bind(gatewayAddress)
 
-while true:
-    print("Listening...")
-    data, addr = gatewaySocket.recvfrom(1024)
-    if "@@@RegisterRequest" in data:
-        handleRegistration(data, addr)
 
-gatewaySocket.close()
 
 
 def handleRegistration(data, addr):
@@ -32,20 +26,28 @@ def handleRegistration(data, addr):
     sessionName = words[2]
 
     matchingSessions = set(s for s in sessions if sessions[s]['SessionName'] == sessionName)
-
-    if matchingSessions:
         
-    if clientType == "client":
-        clientAddr = addr
-        serverAddr = ""
-    elif clientType == "server":
-        clientAddr = ""
-        serverAddr = addr
 
     if not matchingSessions:
+         if clientType == "client":
+            clientAddr = addr
+            serverAddr = ""
+        elif clientType == "server":
+            clientAddr = ""
+            serverAddr = addr
         # This new session is half-complete
-        sessions.add(dict(SessionName=sessionName, ClientAddr=clientAddr, ServerAddr=serverAddr))
+        entry = dict(SessionName=sessionName, ClientAddr=clientAddr, ServerAddr=serverAddr)
+        sessions.add(entry)
     else:
         # This existing session is updated
         thisSession = matchingSessions[0]
 
+if __name__ == "__main__":
+    
+    while(1):
+        print("Listening...")
+        data, addr = gatewaySocket.recvfrom(1024)
+        if "@@@RegisterRequest" in data:
+            handleRegistration(data, addr)
+
+    gatewaySocket.close()
